20229567b167290c4ed7b1fd148b612d0b755667
MrCarambole <131371657+MrCarambole@users.noreply.github.com>
2024-05-30 09:57:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
+echo "Installation du script de sauvegarde 🔨"
+
+echo "[1/7] Installation de sshfs"
 sudo apt-get install sshfs
-mkdir /sftp_mount
-sshfs Administrateur@192.168.1.2:/home/test_backup_augustin/ /sftp_mount
 
-mkdir /sftp_mount/archives
-mkdir /sftp_mount/archives/sauvegarde_semaine
-mkdir /sftp_mount/archives/sauvegarde_mois
+echo "[2/7] Creation du nouveau dossier de backup"
+mkdir /backup.backup
 
-mv /home/yunohost.backup/archives/* /sftp_mount/archives/
-rm -rf /home/yunohost.backup/archives
-ln -s /sftp_mount/archives /home/yunohost.backup/archives
+echo "[3/7] Connexion du nouveau dossier de backup au NAS distant"
+sshfs Administrateur@192.168.1.2:/home/test_backup_augustin /backup.backup
 
+echo "[4/7] Creation de dossier nécessaire au fonctionnement du script"
+mkdir /backup.backup/archives
+mkdir /backup.backup/archives/sauvegarde_semaine
+mkdir /backup.backup/archives/sauvegarde_mois
+
+echo "[5/7] Telechargement du script"
 mkdir /script_backup
-curl "https://raw.githubusercontent.com/MrCarambole/Backcup-s/main/script_v2.py">/script_backup/script.py
+curl "https://raw.githubusercontent.com/MrCarambole/Backcup-s/main/script_v3.py">/script_backup/script.py
 echo "0">/script_backup/cycle.log
-echo "">/script_backup/backup.log
 
-SCRIPT_PATH="/script_backup/script.py"
-(crontab -l ; echo "**/10 * * * * /usr/bin/python3 $SCRIPT_PATH") | crontab -
+echo "[6/7] Modification de l'application YunoHost"
+curl "https://raw.githubusercontent.com/MrCarambole/Backcup-s/main/backup.py">/usr/lib/python3/dist-packages/yunohost/backup.py
+curl "https://raw.githubusercontent.com/MrCarambole/Backcup-s/main/app.py">/usr/lib/python3/dist-packages/yunohost/app.py
+
+echo "[7/7] Gestion de la crontab"
+(crontab -l ; echo "0 21 * * * /usr/bin/python3 /script_backup/script.py") | crontab -
 service cron restart
 
-echo "Configuration terminée."
+echo "Le script de sauvegarde est installé 🐸"
