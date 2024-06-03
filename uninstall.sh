55ab87adef48a73d8afbd1d052db586eb820739d
MrCarambole <131371657+MrCarambole@users.noreply.github.com>
2024-06-03 11:33:41 +0200

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+fusermount -u /backup.backup/
+rm -R /backup.backup/
+rm -R /script_backup/
+rm /setup.sh
+sed -i 's/\/backup\.backup/\/home\/yunohost\.backup/g' /usr/lib/python3/dist-packages/yunohost/backup.py
+sed -i 's/backup\.backup/yunohost\.backup/' /usr/lib/python3/dist-packages/yunohost/app.py
+crontab -l | sed '/\/script_backup\/script.py/d' | crontab -
